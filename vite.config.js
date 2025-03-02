6dc925d449347c58c4a0ecd758cf14d15c31e599
fidjumbo <fidatemie@gmail.com>
2025-03-02 00:56:04 -0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT,
+  },
   plugins: [react(),
     tailwindcss(),
+    vercel(),
   ],
 })
